4b8112495a28ca1cd64ea2128b03796b8d0d7c0a
egmar afonso <78624465+afonsoegmar@users.noreply.github.com>
2023-05-19 17:20:37 -0300

    Primeiro Commit gerenciador de Usuários

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Listagem from "./Componentes/Listagem.js"
 
 function App() {
+
+  // Como funciona objeto em javascript
+  const usuario = {
+    //chave: valor
+    nome:"Egmar",
+    idade: 54,
+    status:true,
+    endereco: {
+      rua: "Londres",
+      numero: 139,
+      complemento:"ap 20"
+    },
+    vacinas_covid19:["Pfizer", "Pfizer", "Pfize", "J&J"]
+  }
+
+  const qnt_vacina = usuario.vacinas_covid19.length - 1
+
+  const usuarios = [
+    
+      {nome: "Egmar", status: true},
+      {nome: "azenha", status: false},
+      {nome: "Doni", status: true},
+      {nome: "Felipe", status: true}
+
+    
+  ]
+
+  console.log(usuarios)
+  //console.log(usuario)
+  //console.log(usuario.endereco.rua, usuario.endereco.complemento)
+  console.log(Object.keys(usuario))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="d-flex justify-content-around">
+        
+        
+        <Listagem usuarios={usuarios}/>
+        <div className="caixa">
+          <h2> Cadastro</h2>
+
+        </div>
+
+      </div >
+      
     </div>
   );
 }
+
+
 
 export default App;
